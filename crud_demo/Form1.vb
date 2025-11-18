e332287e3d5d886504f88ade0e08539fbff31ee0
Lansxz1 <lancebulan1928@gmail.com>
2025-11-18 10:07:09 +0800

    Stuffed

--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -18,5 +18,22 @@ Public Class Form1
 
     Private Sub ButtonInsert_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
 
+        Dim query As String = "INSERT INTO students_tbl (name, age, email) VALUES (@name, @age, @email)"
+
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database= crud_demo_db")
+                conn.Open()
+            End Using
+            Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@name", TextBoxName.Text)
+                    cmd.Parameters.AddWithValue("@age", TextBoxAge.Text)
+                    cmd.Parameters.AddWithValue("@name", TextBoxEmail.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record insert succesfully!")
+
+                End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 End Class
